adcf4e9ae173e03a04866c4cfdf1cd4c03cbd287
vicky <victoriafboix@gmail.com>
2023-02-03 12:56:46 +0200

    Parent to child component

--- force-app/main/default/lwc/employeeDetail/employeeDetail.js
+++ force-app/main/default/lwc/employeeDetail/employeeDetail.js
@@ -1,17 +1,6 @@
-import { LightningElement,track } from 'lwc';
+import { LightningElement,api } from 'lwc';
 
 export default class EmployeeDetail extends LightningElement {
-    @track name;
-    @track age;
-    @track salary;
+    @api empDetail={empName:'Encarna', empAddress:'Mostoles'}
 
-    nameHandler(event) {
-        this.name=event.target.value;
-    }
-    ageHandler(event) {
-        this.age=event.target.value;
-    }
-    salaryHandler(event) {
-        this.salary=event.target.value;
-    }
 }
---
+++ force-app/main/default/lwc/employeeDetails/employeeDetails.js
@@ -0,0 +1,21 @@
+import { LightningElement } from 'lwc';
+
+export default class EmployeeDetails extends LightningElement {
+    empDetails=[
+        {
+            empName:"Steve", empAddress:"NC"
+        },
+
+        {
+            empName:"Pepe", empAddress:"GF"
+        },
+
+        {
+            empName:"Manoli", empAddress:"HJTF"
+        },
+
+        {
+            empName:"Pepica", empAddress:"FR"
+        }
+    ]
+}

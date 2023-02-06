a3c7642a7f2b584260597b11c4ddbbbc58ab3bd5
vicky <victoriafboix@gmail.com>
2023-02-06 13:49:53 +0200

    Component Lifecycle Hooks

--- force-app/main/default/lwc/bool/bool.js
+++ force-app/main/default/lwc/bool/bool.js
@@ -2,4 +2,21 @@ import { LightningElement,api } from 'lwc';
 
 export default class Bool extends LightningElement {
     @api show=false;
+
+    constructor() {
+        super();
+        console.log("construcor on the child is called");
+    }
+
+    connectedCallback() {
+        console.log("connected callback on the child is called");
+    }
+
+    renderedCallback() {
+        console.log("rendered callback on the child is called");
+    }
+
+    disconectedCallback() {
+        console.log("disconected callback on the child is called");
+    }
 }
--- force-app/main/default/lwc/boolParent/boolParent.js
+++ force-app/main/default/lwc/boolParent/boolParent.js
@@ -1,3 +1,21 @@
 import { LightningElement } from 'lwc';
 
-export default class BoolParent extends LightningElement {}
+export default class BoolParent extends LightningElement {
+
+    constructor() {
+        super();
+        console.log("construcor on the parent is called");
+    }
+
+    connectedCallback() {
+        console.log("connected callback on the parent is called");
+    }
+
+    renderedCallback() {
+        console.log("rendered callback on the parent is called");
+    }
+
+    disconectedCallback() {
+        console.log("disconected callback on the parent is called");
+    }
+}

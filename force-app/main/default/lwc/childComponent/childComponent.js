cb392a280e53beef2b9e90c71b33ed1cc1df1053
vicky <victoriafboix@gmail.com>
2023-02-28 14:01:58 +0200

    Subscriber Publisher that are not wotking!

--- force-app/main/default/lwc/childComponent/childComponent.js
+++ force-app/main/default/lwc/childComponent/childComponent.js
@@ -3,7 +3,7 @@ import { LightningElement} from 'lwc';
 export default class ChildComponent extends LightningElement {
     handleChange(event) {
         const name=event.target.value;
-        const selectEvent=new CustomEvent("mycustomevent",{detail:name});
+        const selectEvent=new CustomEvent("mycustomevent",{detail:name,bubbles:true});
         this.dispatchEvent(selectEvent);
         
     }
--- force-app/main/default/lwc/parentComponent/parentComponent.js
+++ force-app/main/default/lwc/parentComponent/parentComponent.js
@@ -2,6 +2,10 @@ import { LightningElement } from 'lwc';
 
 export default class ParentComponent extends LightningElement {
     msg;
+    constructor() {
+        super();
+        this.template.addEventListener("mycustomevent",this.handleCustomEvent.bind(this));
+    }
     handleCustomEvent(event) {
         this.msg=event.detail;
         
